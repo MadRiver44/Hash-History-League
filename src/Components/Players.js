d6487fe5ae193a554243735d4d3a195e8af39b0d
Kevin Turney <super996@optonline.net>
2018-01-25 12:43:29 -0500

    load players on componentDidMount, render Sidebar, query params

---
+++ src/Components/Players.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+import Sidebar from './Sidebar'
+import { getPlayers } from '../api'
+import { parse } from 'query-string'
+import slug from 'slug'
+
+export default class Players extends Component {
+  state = {
+    players: [],
+    loading: true,
+  }
+
+  componentDidMount() {
+    const { location } = this.props
+    // if their is a query param, then render entire list
+    location.search ? this.fetchPlayers(parse(location.search).teamId) : this.fetchPlayers()
+  }
+
+  fetchPlayers = teamId => {
+    getPlayers(teamId).then(players =>
+      this.setState(() => ({
+        loading: false,
+        players,
+      })),
+    )
+  }
+  render() {
+    // {..this.props} Routing props (location, history, and  match go to Sidebar also
+    const { players, loading } = this.state
+    const { match, location } = this.props
+    return (
+      <div className="container two-column">
+        <Sidebar
+          loading={loading}
+          title="Players"
+          list={players.map(player => player.name)}
+          {...this.props}
+        />
+        {loading === false && location.pathname === '/players' ? (
+          <div className="sidebar-instruction">Select a Player</div>
+        ) : null}
+      </div>
+    )
+  }
+}
