0d2b80dd4ae92e0328dc64f42fc580eccf5f726a
Kevin Turney <super996@optonline.net>
2018-01-25 14:26:19 -0500

    find team and create a nested route,

--- src/Components/Teams.js
+++ src/Components/Teams.js
@@ -1,7 +1,53 @@
 import React, { Component } from 'react'
+import Sidebar from './Sidebar'
+import { getTeamNames } from '../api'
+import { Route, Link } from 'react-router-dom'
+import TeamLogo from './TeamLogo'
+import Team from './Team'
 
 export default class Teams extends Component {
+  state = {
+    teamNames: [],
+    loading: true,
+  }
+  componentDidMount() {
+    getTeamNames().then(teamNames => {
+      this.setState(() => ({
+        loading: false,
+        teamNames,
+      }))
+    })
+  }
   render() {
-    return <div className="container two-column"> TEAMS</div>
+    const { loading, teamNames } = this.state
+    const { location, match } = this.props
+
+    return (
+      <div className="container two-column">
+        <Sidebar loading={loading} title="Teams" list={teamNames} {...this.props} />
+
+        {loading === false && location.pathname === '/teams' ? (
+          <div className="sidebar-instruction">Select a Team</div>
+        ) : null}
+
+        <Route
+          path={`${match.url}/:teamId`}
+          render={({ match }) => (
+            <div className="panel">
+              <Team id={match.params.teamId}>
+                {team =>
+                  team === null ? (
+                    <h1>LOADING</h1>
+                  ) : (
+                    <div style={{ width: '100%' }}>
+                      <TeamLogo id={team.id} className="center" />
+                    </div>
+                  )}
+              </Team>
+            </div>
+          )}
+        />
+      </div>
+    )
   }
 }
